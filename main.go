b6ba71aa54c7afddb5ae797c4c36f8e916834942
Luis San Martin <luis@sanmartin.io>
2019-09-18 15:42:11 +0200

    flags for image

--- main.go
+++ main.go
@@ -2,13 +2,15 @@
 // pull-push images from any docker repo. Sends an email if something goes wrong
 // Usage:
 //
-// ./deemon your-registry.domain.tld/yourimage:0.1
+// ./deemon -image your-repo.domain.tld/yourimage:0.1
 
 package main
 
 import (
 	"log"
 	"os"
+
+	"flag"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -17,19 +19,27 @@ import (
 )
 
 func main() {
+	image := flag.String("image", os.Args[1], "docker image you want to pull")
+	flag.Parse()
+	// TODO: we should be able to use different smtp hosts
+	testPull(*image)
+}
+
+func testPull(image string) {
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		log.Println(err)
 	}
 
-	_, err = cli.ImagePull(ctx, os.Args[1], types.ImagePullOptions{})
+	_, err = cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	log.Println("Pulling image", image)
 	if err != nil {
 		log.Println(err)
 		// we should send the error through email
 		sendEmail(err.Error())
 	} else {
-		log.Printf("your-registry.domain.tld is ok")
+		log.Printf("%s is ok", image)
 	}
 
 }
@@ -39,10 +49,10 @@ func sendEmail(msg string) {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "youremail@domain.tld")
 	m.SetHeader("To", "youremail@domain.tld")
-	m.SetHeader("Subject", "[Alert] your-registry.domain.tld")
+	m.SetHeader("Subject", "[Alert] yourregistry.domain.tld")
 	m.SetBody("text/plain", msg)
 
-	d := gomail.Dialer{Host: "your.smtp.dev", Port: 25}
+	d := gomail.Dialer{Host: "smtp.domain.tld", Port: 25}
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
 	}
